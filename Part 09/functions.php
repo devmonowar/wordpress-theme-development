1e4e3823ea8552c5af226c6901bcfd8d9286d4a0
Monowar <kstmonowar@gmail.com>
2022-06-15 11:37:39 +0600

    Create a walker menu

--- Part 09/functions.php
+++ Part 09/functions.php
@@ -84,7 +84,7 @@ add_action('customize_register', 'ali_customizar_register');
 // Menu Register
 register_nav_menu( 'main_menu', __('Main Menu', 'alihossain') );
 
-// Walker Menu Properties
+// Walker Menu Properties //Create a walker menu 
 function ali_nav_description( $item_output, $item, $args){
   if( !empty ($item->description)){
     $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' . $item->description . '</span>' . $args->link_after . '</a>', $item_output);
